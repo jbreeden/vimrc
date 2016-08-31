68b0bdfd7146ea1a918854a2db8e2e1310d871eb
Jared Breeden <jbreeden@splunk.com>
2016-08-30 17:02:23 -0700

    Started updating Rakefile. Will finish at home

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,12 +1,24 @@
-desc "Install my vimrc & plugins"
-task :install do
-  root = File.dirname(__FILE__)
-  ln_s "#{root}/jbreeden.vimrc", File.expand_path("~/.vimrc") rescue nil
-  ln_s "#{root}/jbreeden.vim", File.expand_path("~/.vim") rescue nil
-  cd("#{root}/jbreeden.vim/bundle") do
-    unless Dir.exists?('vim-fugitive')
-      sh 'git clone https://github.com/tpope/vim-fugitive'
-      sh 'vim -u NONE -c "helptags vim-fugitive/doc" -c q'
+$root = File.dirname(__FILE__)
+
+def bundle(folder_name)
+  cd("#{$root}/jbreeden.vim/bundle") do
+    unless Dir.exists?(folder_name)
+      yield
     end
   end
+end
+
+desc "Install my vimrc & plugins"
+task :install do
+  ln_s "#{$root}/jbreeden.vimrc", File.expand_path("~/.vimrc") rescue nil
+  ln_s "#{$root}/jbreeden.vim", File.expand_path("~/.vim") rescue nil
+
+  bundle('vim-fugitive') {
+    sh 'git clone https://github.com/tpope/vim-fugitive'
+    sh 'vim -u NONE -c "helptags vim-fugitive/doc" -c q'
+  }
+
+#  bundle('powerline') {
+#    sh 'git clone https://github.com/powerline/powerline'
+#  }
 end
